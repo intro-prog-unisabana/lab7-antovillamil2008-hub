df65592a4fa41caf21e5b71833aadea14b552eef
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-09 11:23:05 -0500

    parte 2.1

--- password_manager.py
+++ password_manager.py
@@ -9,12 +9,15 @@ def encrypt_single_pass(filename: str) -> None:
     constraseña_encriptada=caesar_encrypt(contraseña)
     with open (filename, "w")as archivo:
         archivo.write(constraseña_encriptada)
+
  
     
 
 def encrypt_passwords_in_file(filename: str) -> None:
     """TODO: Parte 2."""
-    pass
+    with open(filename, "r" ) as archivo: 
+        for linea in archivo: 
+            print(linea)
 
 
 def change_password(filename: str, website: str, password: str) -> bool:
@@ -27,3 +30,5 @@ def add_login(filename: str, website_name: str, username: str, password: str) ->
     pass
 
 encrypt_single_pass('example1.txt')
+
+encrypt_passwords_in_file('examples/example2.csv')
