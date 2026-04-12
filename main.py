8bf2b1f2086e1bc93540a7a4eb6a99168eff7274
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-11 20:01:42 -0500

    avance 5

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from password_manager import add_login, change_password, encrypt_passwords_in_fi
 
 def main() -> None:
     """TODO: Parte 5 - programa principal interactivo."""
-    pass
+    filename= input("Enter the CSV file name:")
+    encrypt_passwords_in_file(filename)
+    while True:
+        print("Options: (1) Change Password, (2) Add Password, (3) Quit:")
+        opcion=input().strip()
+        if opcion=="1":
+            datos=input("Enter the website and the new password:")
+            
 
 
 if __name__ == "__main__":
