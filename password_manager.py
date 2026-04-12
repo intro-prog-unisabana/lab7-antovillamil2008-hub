44a0f118f968496e30e49c9ce3e2db619650080e
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-04-11 19:37:27 -0500

    problema 3

--- password_manager.py
+++ password_manager.py
@@ -6,9 +6,9 @@ def encrypt_single_pass(filename: str) -> None:
     """TODO: Parte 1."""     
     with open(filename, "r") as archivo:  
         contraseña=archivo.read().strip()
-    constraseña_encriptada=caesar_encrypt(contraseña)
+    contraseña_encriptada=caesar_encrypt(contraseña)
     with open (filename, "w")as archivo:
-        archivo.write(constraseña_encriptada)
+        archivo.write(contraseña_encriptada)
 
  
 def encrypt_passwords_in_file(filename: str) -> None:
@@ -28,8 +28,22 @@ def encrypt_passwords_in_file(filename: str) -> None:
 
 def change_password(filename: str, website: str, password: str) -> bool:
     """TODO: Parte 3."""
-    pass
-
+    data=[]
+    encontrado=False
+    with open(filename, "r") as archivo:
+        lector=csv.reader(archivo)
+        for linea in lector:
+            if len(linea) >0:
+                if linea[0]==website:
+                    linea[2]=caesar_encrypt(password)
+                    encontrado=True
+                data.append(linea)
+    if encontrado== False:
+        return False
+    with open(filename, "w", newline='') as archivo:
+        escritor=csv.writer(archivo)
+        escritor.writerows(data)
+        return True
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
     """TODO: Parte 4."""
@@ -38,5 +52,5 @@ def add_login(filename: str, website_name: str, username: str, password: str) ->
 
 if __name__ == "_main_":
     encrypt_single_pass('example1.txt')
-
     encrypt_passwords_in_file('examples/example2.csv')
+    change_password("example3.csv", "nonexistent.com", "randompass")
